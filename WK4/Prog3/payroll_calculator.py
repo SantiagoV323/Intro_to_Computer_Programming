3cc6d78bf13008709c7c7f996f8d809d7615a834
Santiago V <santiago_vega99@hotmail.com>
2026-02-06 13:42:23 -0600

    Checkpoint from VS Code for coding agent session

---
+++ WK4/Prog3/payroll_calculator.py
@@ -0,0 +1,29 @@
+# Andres Santiago Vega Franco T-20331228
+# 02/05/2026
+
+# TODO Briefily describe the program
+
+def main ():
+    hours = [6.75, 8.25, 10.75, 0.0, 8.25]
+    print ('')
+    first_name = input ('Enter the first name of the employee: ')
+    last_name = input ('Enter the last name of the employee: ')
+
+    # Asking the user for the hourly wage, featuring the employee's name for a more personalized experience.
+    hourly_wage = float (input (f"Enter the hourly wage for {first_name} {last_name}: "))
+
+    saturdays = float(input ('Enter the number of hours worked on Saturdays: '))
+    # Adding the hours worked on Saturdays to the list hours.
+    hours.append (float (saturdays))
+    # Calculating the total of hours worked by summing the elements in the list hours.
+    total_hours = sum (hours)
+    # Calculating the total earnings by multiplying the total hours worked by the hourly wage
+    total_earnings = total_hours * hourly_wage
+    # remove the (single) zero hours day
+    hours.remove (0.0)
+    # Number of days worked
+    days_worked = len (hours)
+
+    print (f'{first_name} {last_name} has worked: {days_worked} days, a total of {total_hours:.2f} hours, with a wage of {hourly_wage:.2f} per hour, for a total of $ {total_earnings:.2f}')
+
+main ()
